813393c0c396535d62660f79006b77ec49e0a512
HRiver <sanscout@naver.com>
2024-03-20 11:48:17 +0900

    first commit

---
+++ src/main/java/com/ssg/ssglandersmini2/Ssglandersmini2Application.java
@@ -0,0 +1,13 @@
+package com.ssg.ssglandersmini2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssglandersmini2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssglandersmini2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssg/ssglandersmini2/Ssglandersmini2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssg.ssglandersmini2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssglandersmini2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
