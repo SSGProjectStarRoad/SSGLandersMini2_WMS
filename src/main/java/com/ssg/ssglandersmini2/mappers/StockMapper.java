2a4ff1d5aa61feb36c33c4abe3f3dedd4f58ffd3
MSH0512 <wndqhr00@gmail.com>
2024-03-26 10:58:51 +0900

    #1 기능구현 완료

--- src/main/java/com/ssg/ssglandersmini2/mappers/StockMapper.java
+++ src/main/java/com/ssg/ssglandersmini2/mappers/StockMapper.java
@@ -1,4 +1,15 @@
 package com.ssg.ssglandersmini2.mappers;
 
+import com.ssg.ssglandersmini2.dto.PageRequestDTO;
+import com.ssg.ssglandersmini2.dto.StockDTO;
+
+import java.util.List;
+
 public interface StockMapper {
+    List<StockDTO> getWarehouse();
+    List<StockDTO> selectAll();
+    int getCount(PageRequestDTO pageRequestDTO);
+    List<StockDTO> selectList(PageRequestDTO pageRequestDTO);
+
+
 }
